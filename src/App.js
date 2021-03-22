63c64556dbc18d0716a7a848636acd31f61147f8
Jordy Pena <jordypenamontes@gmail.com>
2021-03-22 17:35:37 -0500

    app.js useeffect fetching data from api setting data into state passing to home component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import Home from './components/Home';
-import Nav from './components/Nav';
-import About from './components/About';
-import { Route } from 'react-router-dom';
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import { Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [currentCards, setCurrentCards] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.pokemontcg.io/v2/cards?q=!name:charizard`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCurrentCards(data.data);
+        console.log("app.js", data.data);
+      });
+  }, []);
+
   return (
     <>
-      <Route path='/' component={Nav}/>
+      <Route path="/" component={Nav} />
 
-      <Route exact path='/' component={Home}/>
-
-      <Route exact path='/about' component={About}/>
+      <Route
+        exact
+        path="/"
+        render={(props) => <Home cardsData={currentCards} />}
+      />
+      <Route exact path="/about" component={About} />
     </>
   );
 }
