103e755331e47baab1e7eed9381e3bbc590fd98d
Jordy Pena <67558072+JordyPena@users.noreply.github.com>
2021-03-22 21:05:58 -0500

    Dynamic (#5)
    * basic styling added
    
    * summary.js
    added card img
    
    * home.js
    removed summary component
    
    * card.js
    added link around card img
    -dynamic link to matching id
    
    * app.js
    - created dynamic path for summary

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,13 @@
 import "../styling/card.css";
+import { Link } from "react-router-dom";
 
-export default function Card({ card }) {
+export default function Card({ card, match }) {
   return (
     <>
       <div className="card">
-        <img src={card.images.small} alt="pokemon" />
+        <Link to={`card-summary/${card.id}`}>
+          <img src={card.images.small} alt="pokemon" />
+        </Link>
       </div>
     </>
   );
