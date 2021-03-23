2c130f949621b668da51b3e81c96c840ff32b5cf
Jordy Pena <jordypenamontes@gmail.com>
2021-03-22 21:04:10 -0500

    home.js removed summary component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,16 @@
 import Card from "./Card";
-import Summary from "./Summary";
 import "../styling/home.css";
-export default function Home({ cardsData }) {
+export default function Home({ cardsData, match }) {
   return (
     <>
-    
-        <div className="row">
-          <div className="left">
-            {console.log(cardsData)}
-            {cardsData.map((card) => {
-              return <Card card={card} />;
-            })}
-          </div>
-
-          <div className="right">
-            {cardsData.map((card) => {
-              return <Summary card={card} />;
-            })}
-          </div>
+      <div className="row">
+        <div className="left">
+          {console.log(cardsData)}
+          {cardsData.map((card) => {
+            return <Card card={card} match={match} />;
+          })}
         </div>
-    
+      </div>
     </>
   );
 }
