ee1763105c86728fd304a286ecac9acb44c2283e
Jordy Pena <jordypenamontes@gmail.com>
2021-04-12 21:26:01 -0500

    -themes.js saved current theme to localStorage

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -5,11 +5,38 @@ import { BsSun } from "react-icons/bs";
 
 export default function Themes() {
   const [darkMode, setDarkMode] = useState(false);
+  const [lightMode, setLightMode] = useState(true);
 
   useEffect(() => {
     const bodyElement = document.body;
     if (darkMode) bodyElement.classList.add("dark-mode");
     else bodyElement.classList.remove("dark-mode");
+  }, [darkMode]);
+
+  useEffect(() => {
+    if (localStorage.getItem("darkMode")) setDarkMode(true);
+    else return;
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem("lightMode")) setLightMode(true);
+    else return;
+  }, []);
+
+  useEffect(() => {
+    if (lightMode === true)
+      localStorage.setItem("lightMode", JSON.stringify(lightMode));
+    else localStorage.removeItem("lightMode");
+  }, [lightMode]);
+
+  useEffect(() => {
+    if (darkMode === true) {
+      localStorage.setItem("darkMode", JSON.stringify(darkMode));
+      setLightMode(false);
+    } else if (darkMode === false) {
+      localStorage.removeItem("darkMode");
+      setLightMode(true);
+    }
   }, [darkMode]);
 
   return (
