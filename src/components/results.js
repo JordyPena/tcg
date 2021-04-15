37c6524d18ae25754c4d48984af7dc530dc85aea
Jordy Pena <jordypenamontes@gmail.com>
2021-04-15 18:16:55 -0500

    results.js added conditionals to send user to page 1 if not on page 1

--- src/components/results.js
+++ src/components/results.js
@@ -1,26 +1,24 @@
-import Card from '../components/Card';
-import Pagination from '../components/Pagination';
+import Card from "../components/Card";
+import Pagination from "../components/Pagination";
 import Footer from "../components/Footer";
 import { useState, useEffect } from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 export default function Result({ match }) {
-
   const [pokemonData, setPokemonData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [invalidSearch, setInvalidSearch] = useState(false);
-  const [ numberOfPages,  setNumberOfPages] = useState(0);
+  const [numberOfPages, setNumberOfPages] = useState(0);
 
   const history = useHistory();
 
   useEffect(() => {
+    const { name, page, pageSize, orderBy, desc } = match.params;
 
-    const { name, page, pageSize } = match.params
+    console.log(match.params);
+    let url = `https://api.pokemontcg.io/v2/cards?q=name:${name}&page=${page}&pageSize=${pageSize}&orderBy=${orderBy}&desc=${desc}`;
 
-    console.log(match.params.orderBy)
-    let url =`https://api.pokemontcg.io/v2/cards?q=name:${name}&page=${page}&pageSize=${pageSize || "25"}`
-
-    let orderBy;
+    let orderByText;
 
     // case is what is gonna show in my url to the user
     // so when i have released im saying if my url is displaying and param as released
@@ -28,15 +26,15 @@ export default function Result({ match }) {
     //data correctly from my fetch but can display something different to my user
     // if nothing meets my case return what the url is
     if (match.params.orderBy) {
-      switch(match.params.orderBy) {
-        case 'released':
-          orderBy = 'set.releaseDate';
+      switch (match.params.orderBy) {
+        case "released":
+          orderByText = "set.releaseDate";
           break;
-        case 'set':
-          orderBy = 'set.name,number';
+        case "set":
+          orderByText = "set.name,number";
           break;
         default:
-          orderBy = match.params.orderBy
+          orderByText = match.params.orderBy;
           break;
       }
 
@@ -44,117 +42,154 @@ export default function Result({ match }) {
       // take my orderBy var and split it and map each value
       // and return that value with a minus sign in front of it
       // join the values together and create a new url to fetch with the new params added to the end
-      if (match.params.desc) {
-       
-        orderBy = orderBy.split(',').map((eachValue) => {
-        
-          return `-${eachValue}`
-        })
-        orderBy.join('')
+      if (match.params.desc === "Desc") {
+        orderByText = orderByText.split(",").map((eachValue) => {
+          return `-${eachValue}`;
+        });
+        orderByText.join("");
       }
-         let orderByParam = `orderBy=${orderBy}`
+      let orderByParam = `orderBy=${orderByText}`;
 
-        url = url + "&" + orderByParam
-    
+      url = url + "&" + orderByParam;
     }
 
     fetch(url)
-    .then((response) => {
-      setLoading(true);
-      if (response.status === 400) {
-        setInvalidSearch(true);
-        throw new Error("Type a valid pokemon name in field");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      setNumberOfPages(Math.ceil(data.totalCount / data.pageSize))
-      console.log("all data",data)
-      setInvalidSearch(false);
-      setPokemonData(data.data);
-      setLoading(false);
-    })
-    .catch((error) => {
-      console.error({ error });
-    });
+      .then((response) => {
+        setLoading(true);
+        if (response.status === 400) {
+          setInvalidSearch(true);
+          throw new Error("Type a valid pokemon name in field");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setNumberOfPages(Math.ceil(data.totalCount / data.pageSize));
+        console.log("all data", data);
+        setInvalidSearch(false);
+        setPokemonData(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error({ error });
+      });
   }, [match]);
 
   if (loading) {
-    return <h2>Loading....</h2>
-  }
-  
-  const paginate = (pageNumber, pokemonName, cardsPerPage, orderCardsBy, ascOrDesc) => {
-    window.scrollTo(0, 0);
-    history.push(`/cards/${pokemonName}/${pageNumber}/${cardsPerPage}/${orderCardsBy}/${ascOrDesc}`)
-  }
-  
-// this is for sortBy param
-  const selectedOption = (event) => {
- 
-    history.push(`/cards/${match.params.name}/${match.params.page}/${match.params.pageSize || "25"}/${event.target.value}`)
+    return <h2>Loading....</h2>;
   }
 
-  // pushing to a new path with all my params 
+  const paginate = (
+    pageNumber,
+    pokemonName,
+    cardsPerPage,
+    orderCardsBy,
+    ascOrDesc
+  ) => {
+    window.scrollTo(0, 0);
+    history.push(
+      `/cards/${pokemonName}/${pageNumber}/${cardsPerPage}/${orderCardsBy}/${ascOrDesc}`
+    );
+  };
+
+  // this is for sortBy param
+  const selectedOption = (event) => {
+    if (match.params.page !== "1") {
+      history.push(
+        `/cards/${match.params.name}/1/${match.params.pageSize}/${event.target.value}/${match.params.desc}`
+      );
+    } else
+      history.push(
+        `/cards/${match.params.name}/${match.params.page}/${match.params.pageSize}/${event.target.value}/${match.params.desc}`
+      );
+  };
+
+  // pushing to a new path with all my params
   //if the value of the option is Desc
   const selectedSortOrder = (event) => {
-  
-    let url = `/cards/${match.params.name}/${match.params.page}/${match.params.pageSize || "25"}/${match.params.orderBy || "name"}`
-    if (event.target.value === "Desc") 
-      url += '/Desc'
-    history.push(url)   
-  }
+    if (match.params.page !== "1") {
+      let url = `/cards/${match.params.name}/1/${match.params.pageSize}/${match.params.orderBy}`;
+      if (event.target.value === "Desc") {
+        url += "/Desc";
+        history.push(url);
+      } else url += "/Asc";
+      history.push(url);
+    } 
+    
+    else if (match.params.page === "1"){
+      let url = `/cards/${match.params.name}/${match.params.page}/${match.params.pageSize}/${match.params.orderBy}`;
+      if (event.target.value === "Desc") {
+        url +="/Desc"
+        history.push(url)
+      } else url += "/Asc"
+      history.push(url);
+    }
+  };
 
-// this is for pageSize param
+  // this is for pageSize param
   const selectedPageSize = ({ target }) => {
-   console.log(target.value)
-    let url = `/cards/${match.params.name}/${match.params.page}/${target.value || "25"}/${match.params.orderBy || "name"}/${match.params.desc || "Asc"}`
-    history.push(url)
-    console.log("Problem", url)
-  }
+    if (match.params.page !== "1") {
+      let url = `/cards/${match.params.name}/1/${target.value}/${match.params.orderBy}/${match.params.desc}`;
+      history.push(url);
+    } else {
+      let url = `/cards/${match.params.name}/${match.params.page}/${target.value}/${match.params.orderBy}/${match.params.desc}`;
+      history.push(url);
+    }
+  };
 
   return (
-    
     <div className="home-container">
-       <div>
-         {/* either render value if the orderBy param exist or "name" */}
-        <select value={match.params.orderBy || "name"} onChange={(event) => selectedOption(event)}>
+      <div>
+        {/* either render value if the orderBy param exist or "name" */}
+        <select
+          value={match.params.orderBy}
+          onChange={(event) => selectedOption(event)}
+        >
           <option value="name">Name</option>
           <option value="released">Release Date</option>
           <option value="set">Set/Number</option>
-          <option value="rarity">Rarity</option> 
-        </select>  
-       </div>
-       <div>
-         <select value={match.params.desc || "Asc"} onChange={(event) => selectedSortOrder(event)}>
-           <option value="Asc">Asc</option>
-           <option value="Desc">Desc</option>
-         </select>
-       </div>
-       <div>
-         <select value={match.params.pageSize || "25"} onChange={(event) => selectedPageSize(event)}>
-           <option value="5">5</option>
-           <option value="10">10</option>
-           <option value="15">15</option>
-           <option value="20">20</option>
-           <option value="25">25</option>
-         </select>
-       </div>
-        <div className="card">
-          {Object.keys(pokemonData.length) && (
+          <option value="rarity">Rarity</option>
+        </select>
+      </div>
+      <div>
+        <select
+          value={match.params.desc}
+          onChange={(event) => selectedSortOrder(event)}
+        >
+          <option value="Asc">Asc</option>
+          <option value="Desc">Desc</option>
+        </select>
+      </div>
+      <div>
+        <select
+          value={match.params.pageSize}
+          onChange={(event) => selectedPageSize(event)}
+        >
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+          <option value="25">25</option>
+        </select>
+      </div>
+      <div className="card">
+        {Object.keys(pokemonData.length) && (
           <>
             {pokemonData.map((card, index) => {
               return <Card card={card} match={match} key={index} />;
             })}
           </>
-          )}
-        </div>
-        
-        
-       <Pagination numberOfPages={numberOfPages} paginate={paginate} pokemonName={match.params.name}
-       cardsPerPage={match.params.pageSize}
-       orderCardsBy={match.params.orderBy}
-       ascOrDesc={match.params.desc} />
-       <Footer/>
+        )}
       </div>
-  )
+
+      <Pagination
+        numberOfPages={numberOfPages}
+        paginate={paginate}
+        pokemonName={match.params.name}
+        cardsPerPage={match.params.pageSize}
+        orderCardsBy={match.params.orderBy}
+        ascOrDesc={match.params.desc}
+      />
+      <Footer />
+    </div>
+  );
 }
